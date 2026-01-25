06851fb9a1b6eb079528245962e9674aca5a74bb
PhucBITC <phucxo262@gmail.com>
2026-01-25 15:50:48 +0700

    first Project commit1

---
+++ clothingsystem/src/main/java/com/ecommerShop/clothingsystem/ClothingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerShop.clothingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingsystemApplication.class, args);
+	}
+
+}
---
+++ clothingsystem/src/test/java/com/ecommerShop/clothingsystem/ClothingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerShop.clothingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
